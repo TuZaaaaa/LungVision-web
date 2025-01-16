5e53303263615a7955e71f9a2d71531f9c5828eb
TuZaaaaa <2677824148wjt@gmail.com>
2025-01-16 21:32:55 +0800

    chore: add reset css

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import {createPinia} from "pinia";
 import axiosInstance from '@/services/request.js';
 import ElementPlus from 'element-plus'
+
+import '@/assets/styles/reset.css'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
