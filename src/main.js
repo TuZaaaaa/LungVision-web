3f2355d57a072348ca2af8af123c2e29789c5f66
TuZaaaaa <2677824148wjt@gmail.com>
2025-01-16 10:39:08 +0800

    chore: add ElementPlus

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import App from './App.vue'
 import router from './router'
 import {createPinia} from "pinia";
 import axiosInstance from '@/services/request.js';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(router)
+app.use(ElementPlus)
 app.use(pinia)
 
 // 全局挂载 axios 实例
